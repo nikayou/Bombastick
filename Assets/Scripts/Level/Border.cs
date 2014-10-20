ec4376c9af3dd55e6f19508d1027d72f1e225c41
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-10-20 16:47:44 +0200

    Border is automatically created
    Signed-off-by: Nicolas Cailloux <caillouxnicolas@gmail.com>

--- Assets/Scripts/Level/Border.cs
+++ Assets/Scripts/Level/Border.cs
@@ -1,85 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
-/*
-  public class Border : MonoBehaviour {
 
-    public int dimension = 32;
-    public GameObject borderPrefab;
-    private Tile [,] map;
-    private Transform borderContainer;
-    private Transform blocksContainer;
-    private int limit;
+public class Border : MonoBehaviour {
 
-    void Awake () {
-      map = new Tile [dimension, dimension];
-      limit = dimension - 1;
-      borderContainer = transform.Find("Border");
-      blocksContainer = transform.Find("Blocks");
-    }
+  public int dimension = 16;
+  public GameObject borderPrefab;
+  private Transform borderContainer;
+  private Tile [,] map;
+  private int limit;
 
-    void Start () {
-      CreateBounds();
-      CreateRandom();
-      PlaceTiles();
-    }
-
-    void CreateBounds () {
-      for (int i = 0; i < dimension; i++) {
-	map[0, i] = Tile.BLOCK;
-	map[limit, i] = Tile.BLOCK;
-	map[i, 0] = Tile.BLOCK;
-	map[i, limit] = Tile.BLOCK;
-      }
-    }
-
-    void CreateRandom () {
-      int nb = Random.Range(minBlocks, maxBlocks+1);
-      for (int i = 0; i < nb; i++) {
-	int x = Random.Range(1, limit);
-	int y = Random.Range(1, limit);
-	if (!IsSpawn(x, y)) {
-	  map[x,y] = (Tile)(Random.Range(1, (int)Tile.NB));
-	  //	map[x,y] = Tile.DESTRUCT;
-	}
-      }
-    }
-
-    void PlaceTiles () {
-      for (int i = 0; i < dimension; i++) {
-	for (int j = 0; j < dimension; j++) {
-	  if (map[i,j] == Tile.DESTRUCT) {
-	    PlaceBlock(i, j);
-	  } else if (map[i,j] == Tile.BLOCK) {
-	    PlaceBorder(i, j);
-	  }
-	}
-      }
-    }
-
-    void PlaceBlock (int x, int y) {
-      GameObject tile = Instantiate(blockPrefab) as GameObject;
-      tile.transform.position = new Vector3(x, y, 0);
-      tile.transform.parent = blocksContainer;
-      tile.transform.tag = "Destructable";
-    }
-
-    void PlaceBorder (int x, int y) {
-      GameObject tile = Instantiate(borderPrefab) as GameObject;
-      // TODO    
-      //    tile.transform.isStatic = true;
-      tile.transform.position = new Vector3(x, y, 0);
-      tile.transform.parent = borderContainer;
-    }
-
-    bool IsSpawn (int x, int y) {
-      return ( (x <= 3 && y <= 3) 
-	       || (x <= 3 && y >= dimension-4)
-	       || (x >= dimension-4 && y <= 3)
-	       || (x >= dimension-4 && y >= dimension-4) 
-	       // last is for the object
-	       || (x == dimension/2 && y == dimension/2)
-	       );
-    }
-
+  void Awake () {
+    map = new Tile [dimension, dimension];
+    limit = dimension - 1;
+    borderContainer = transform.Find("Border");
   }
-*/
+
+  void Start () {
+    CreateBounds();
+  }
+
+  void CreateBounds () {
+    for (int i = 0; i < dimension; i++) {
+      map[0, i] = Tile.BLOCK;
+      map[limit, i] = Tile.BLOCK;
+      map[i, 0] = Tile.BLOCK;
+      map[i, limit] = Tile.BLOCK;
+      PlaceBorder (0, i);
+      PlaceBorder (limit, i);
+      PlaceBorder (i, 0);
+      PlaceBorder (i, limit);
+    }
+  }
+
+  void PlaceBorder (int x, int y) {
+    GameObject tile = Instantiate(borderPrefab) as GameObject;
+    tile.isStatic = true;
+    tile.transform.position = new Vector3(x, y, 0);
+    tile.transform.parent = borderContainer;
+  }
+
+}
--- Assets/Scripts/Level/LevelEditor.cs
+++ Assets/Scripts/Level/LevelEditor.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class LevelEditor : MonoBehaviour {
 
+void OnGUI () {
 
+}
 
 }
