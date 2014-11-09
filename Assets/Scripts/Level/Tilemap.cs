0fa99897689fd59b0efb9496c145a91aee10f379
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-11-09 20:02:45 +0100

    indented with monoDevelop
    Signed-off-by: Nicolas Cailloux <caillouxnicolas@gmail.com>

--- Assets/Scripts/Level/Tilemap.cs
+++ Assets/Scripts/Level/Tilemap.cs
@@ -3,82 +3,92 @@ using System.Collections;
 using System.IO;
 using System.Xml;
 
-public class Tilemap {
+public class Tilemap
+{
 
-  private int [] layers;
-  private int nbLayers;
-  private int width;
-  private int height; //TODO: check if required to keep it
-  private int nbPerLayer;
+		private int[] layers;
+		private int nbLayers;
+		private int width;
+		private int height; //TODO: check if required to keep it
+		private int nbPerLayer;
 
-  public Tilemap (int _width, int _height, int _nbLayers = 2) {
-    width = _width;
-    height = _height;
-    nbPerLayer = width * height;
-    nbLayers = _nbLayers;
-    layers = new int [nbLayers * nbPerLayer];
-  }
+		public Tilemap (int _width, int _height, int _nbLayers = 2)
+		{
+				width = _width;
+				height = _height;
+				nbPerLayer = width * height;
+				nbLayers = _nbLayers;
+				layers = new int [nbLayers * nbPerLayer];
+		}
 
-  public Tilemap (string path) {
-    LoadFromXML (path);
-  }
+		public Tilemap (string path)
+		{
+				LoadFromXML (path);
+		}
 
-  void LoadFromXML (string path) {
-    XmlTextReader xtr = new XmlTextReader (path);
-    while(xtr.Read()) {
-      if (xtr.Name == "MAP") {
-	string widthString = string.Empty;
-	if ((widthString = xtr.GetAttribute("width")) != null) {
-	  width = int.Parse(widthString);
-	}
-	string heightString = string.Empty;
-	if ((heightString = xtr.GetAttribute("height")) != null) {
-	  height = int.Parse(heightString);
-	}
-	string layersString = string.Empty;
-	if ((layersString = xtr.GetAttribute("nbLayers")) != null) {
-	  nbLayers = int.Parse(layersString);
-	}
-	nbPerLayer = width * height;
-	layers = new int [nbLayers * nbPerLayer];
-      } 
-    }
-  }
+		void LoadFromXML (string path)
+		{
+				XmlTextReader xtr = new XmlTextReader (path);
+				while (xtr.Read()) {
+						if (xtr.Name == "MAP") {
+								string widthString = string.Empty;
+								if ((widthString = xtr.GetAttribute ("width")) != null) {
+										width = int.Parse (widthString);
+								}
+								string heightString = string.Empty;
+								if ((heightString = xtr.GetAttribute ("height")) != null) {
+										height = int.Parse (heightString);
+								}
+								string layersString = string.Empty;
+								if ((layersString = xtr.GetAttribute ("nbLayers")) != null) {
+										nbLayers = int.Parse (layersString);
+								}
+								nbPerLayer = width * height;
+								layers = new int [nbLayers * nbPerLayer];
+						} 
+				}
+		}
 
-  public void Put (int id, int x, int y, int layerIndex = 0) {
-    // Assert (x < width);
-    // Assert (y < height);
-    // Assert (layerIndex < nbLayers);
-    layers[(layerIndex * nbPerLayer) + CoordinatesToIndex(x,y)] = id;
-  }
+		public void Put (int id, int x, int y, int layerIndex = 0)
+		{
+				// Assert (x < width);
+				// Assert (y < height);
+				// Assert (layerIndex < nbLayers);
+				layers [(layerIndex * nbPerLayer) + CoordinatesToIndex (x, y)] = id;
+		}
 
-  public int Get (int x, int y, int layerIndex = 0) {
-    // Assert (x < width);
-    // Assert (y < height);
-    // Assert (layerIndex < nbLayers);
-    return layers[(layerIndex * nbPerLayer) + CoordinatesToIndex(x,y)];
-  }
+		public int Get (int x, int y, int layerIndex = 0)
+		{
+				// Assert (x < width);
+				// Assert (y < height);
+				// Assert (layerIndex < nbLayers);
+				return layers [(layerIndex * nbPerLayer) + CoordinatesToIndex (x, y)];
+		}
 
-  public int GetWidth () {
-    return width;
-  }
+		public int GetWidth ()
+		{
+				return width;
+		}
 
-  public int GetHeight () {
-    return height;
-  }
+		public int GetHeight ()
+		{
+				return height;
+		}
 
-  public int GetNbLayers () {
-    return nbLayers;
-  }
+		public int GetNbLayers ()
+		{
+				return nbLayers;
+		}
 
-  int CoordinatesToIndex (int x, int y) {
-    // Assert (x < width);
-    // Assert (y < height);
-    return (y * width + x);
-  }
+		int CoordinatesToIndex (int x, int y)
+		{
+				// Assert (x < width);
+				// Assert (y < height);
+				return (y * width + x);
+		}
 
 
-  /*
+		/*
   void CreateBounds () {
     for (int i = 0; i < dimension; i++) {
       map[0, i] = Tile.BLOCK;
