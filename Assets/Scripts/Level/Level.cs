2a14d62b68c298727aa399ab776652ac929824d8
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-12-23 11:29:12 +0100

    borders are created (with a prefab)
    Signed-off-by: Nicolas Cailloux <caillouxnicolas@gmail.com>

--- Assets/Scripts/Level/Level.cs
+++ Assets/Scripts/Level/Level.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Level : MonoBehaviour
 {
@@ -7,31 +8,35 @@ public class Level : MonoBehaviour
   public Tileset tileset;
   public Tilemap map;
   public GameObject tilePrefab;
+  public GameObject borderPrefab;
   static public Vector2 halfVec = Vector2.one / 2;
   private int size;
-  public int zoom = 4;
+  private Dictionary <int, Sprite> sprites;
 
   public void Create ()
   {
     size = tileset.GetTileSize ();
+    sprites = new Dictionary<int, Sprite> ();
     int stop = map.GetNbLayers ();
+    Transform tilesObject = transform.Find ("Tiles");
     for (int i = 0; i < stop; i++) {
-      CreateFloor (i);
+      CreateFloor (i, tilesObject);
     }
+    CreateBorder (map.GetWidth (), map.GetHeight ());
   }
 
   public void Destroy ()
   {
+    sprites.Clear ();
     Destroy (this.gameObject);
   }
 
-  void CreateFloor (int layer)
+  void CreateFloor (int layer, Transform tilesObject)
   {
     int i, j; // iterators for the map
     int w = map.GetWidth ();
     int h = map.GetHeight ();
     // TODO: don't compute this everytime
-    Transform tilesObject = transform.Find ("Tiles");
     GameObject layerObject = new GameObject ();
     layerObject.transform.parent = tilesObject;
     layerObject.transform.localScale = Vector3.one;
@@ -74,9 +79,37 @@ public class Level : MonoBehaviour
     // assigning sub-sprite
     //TODO : several tiles share the same sprite, no need to create it everytime
     SpriteRenderer sr = (SpriteRenderer)go.renderer;
-    sr.sprite = (Sprite.Create (tileset.GetTexture (), subRect, halfVec, size));
+    Sprite sp;
+    if (! sprites.TryGetValue (tileIndex, out sp)) {
+      // if index has no associated sprite, creating and adding it
+      sp = Sprite.Create (tileset.GetTexture (), subRect, halfVec, size);
+      sprites.Add (tileIndex, sp);
+    }
+    sr.sprite = sp;
     // TODO: colliders and destruction
     return go;
+  }
+
+  void CreateBorder (int w, int h)
+  {
+    Transform borderContainer = transform.FindChild ("Border");
+    for (int x = -1; x <= w; x++) {
+      PlaceBorder (x, 0, borderContainer);
+      PlaceBorder (x, h + 1, borderContainer);
+      int x1 = x + 1;
+      PlaceBorder (-1, x1, borderContainer);
+      PlaceBorder (w, x1, borderContainer);
+    }
+  }
+
+  void PlaceBorder (float x, float y, Transform parent)
+  {
+    GameObject tile = Instantiate (borderPrefab) as GameObject;
+    tile.transform.parent = parent;
+    // TODO    
+    //    tile.transform.isStatic = true;
+    tile.transform.localScale = Vector3.one;
+    tile.transform.localPosition = new Vector3 (x, y, 0);
   }
 
 }
