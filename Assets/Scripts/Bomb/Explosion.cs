bc92b30850958338b02791fdd7803a876fa08862
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-12-26 18:53:25 +0100

    explosions destroy what they are supposed to

--- Assets/Scripts/Bomb/Explosion.cs
+++ Assets/Scripts/Bomb/Explosion.cs
@@ -13,20 +13,40 @@ public class Explosion : MonoBehaviour
   public float scale = 64f;
   public float range = 1f;
   public int mask = 8;
+  private Vector2 rightLine;
+  private Vector2 leftLine;
+  private Vector2 topLine;
+  private Vector2 bottomLine;
 
   void Start ()
   {
+    range += 0.475f;
     mask = 1 << mask;
+    Vector2 two = Vector2.one * 2f;
+    leftLine = -Vector2.right * scale / 2f + two;
+    bottomLine = -Vector2.up * scale / 2f + two;
+    rightLine = Vector2.right * scale / 2f - two;
+    topLine = Vector2.up * scale / 2f - two;
     audio.PlayOneShot (sounds [Random.Range (0, sounds.Length)]);
     if (GameObject.Find ("MatchController").GetComponent<DeathMatch> () != null) {
       deathMatch = true;
     }
     Destroy (transform.parent.gameObject, explosionTime);
-    //TODO: more rays, those only start from the center, we also need upper/lower
-    //Note: those additional rays can take account of the player only 
-    RaycastTo (Vector2.up);
+    Raycasting ();
+  }
+
+  void Raycasting () {
+    RaycastForPlayers (Vector2.up, leftLine);
+    RaycastForPlayers (Vector2.up, rightLine);
+    RaycastTo (Vector2.up);    
+    RaycastForPlayers (-Vector2.up, leftLine);
+    RaycastForPlayers (-Vector2.up, rightLine);
     RaycastTo (-Vector2.up);
+    RaycastForPlayers (Vector2.right, topLine);
+    RaycastForPlayers (Vector2.right, bottomLine);
     RaycastTo (Vector2.right);
+    RaycastForPlayers (-Vector2.right, topLine);
+    RaycastForPlayers (-Vector2.right, bottomLine);
     RaycastTo (-Vector2.right);
   }
 
@@ -46,6 +66,23 @@ public class Explosion : MonoBehaviour
         } else if (hit.collider.gameObject.tag == "Player") {
           KillPlayer (hit.collider.gameObject);
         } 
+      }
+    }
+  }
+
+  void RaycastForPlayers (Vector2 direction, Vector2 shift)
+  {
+    float distance = range * scale;
+    Vector2 origin = (Vector2)transform.position + shift;
+    RaycastHit2D [] hits = Physics2D.RaycastAll (origin, direction, distance, mask);
+    Debug.DrawLine (origin, origin + (direction * distance), Color.cyan);
+    foreach (RaycastHit2D hit in hits) {
+      if (hit.collider.gameObject.tag == "Block") {
+        break;
+      } else if (hit.collider.gameObject.tag == "Destructable") {
+        break;
+      } else if (hit.collider.gameObject.tag == "Player") {
+          KillPlayer (hit.collider.gameObject);
       }
     }
   }
