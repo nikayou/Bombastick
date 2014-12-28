3e248e1dcb7910294cd7960046ca679220e60ddc
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-12-28 20:13:41 +0100

    title's bomb has sound

--- Assets/Scripts/Bomb/ExplosionVisual.cs
+++ Assets/Scripts/Bomb/ExplosionVisual.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(AudioSource))]
+
 public class ExplosionVisual : MonoBehaviour
 {
 
   public float explosionTime = 1.0f;
- 
+  public AudioClip sound;
+
   void Start ()
   {
+    audio.PlayOneShot (sound);
     Destroy (transform.parent.gameObject, explosionTime);
   }
 
