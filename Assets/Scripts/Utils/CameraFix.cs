6ef9166089c26989feaa49ce749aad8688aa82cc
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-12-28 21:36:02 +0100

    final touch before 1st release

--- Assets/Scripts/Utils/CameraFix.cs
+++ Assets/Scripts/Utils/CameraFix.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class CameraFix : MonoBehaviour
 {
 
+ // public float size = 240f;
+
   // Use this for initialization
   void Start ()
   {
-    float halfH = Screen.height / 2f;
-    Camera.main.orthographicSize = halfH;
-    Camera.main.transform.position = new Vector3 (Screen.width / 2f, halfH, 0f);
+    float halfHeight = Screen.height / 2f;
+    Camera.main.orthographicSize = halfHeight;
+    Camera.main.transform.position = new Vector3 (Screen.width / 2f, halfHeight, 0f);
+    Destroy (this);
   }
  
 }
